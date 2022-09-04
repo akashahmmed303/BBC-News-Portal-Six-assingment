05e105ee91cffdc56cff6c3ba18b7eeb3fb5bb23
akashahmmed303 <ahmedakash336@gmail.com>
2022-09-04 22:12:56 +0600

    catch added

--- js/news.js
+++ js/news.js
@@ -2,7 +2,8 @@ const loadMainApi = () => {
   const url = `https://openapi.programming-hero.com/api/news/categories`;
   fetch(url)
     .then(res => res.json())
-    .then(data => displayMainCategory(data.data.news_category));
+    .then(data => displayMainCategory(data.data.news_category))
+    .catch(error => console.log(error));
 };
 const displayMainCategory = category => {
   // console.log(category);
